109ff03d60549227c0d8053b802e5ad44a9e63cc
Kacper Martela <kacper99@hotmail.co.uk>
2019-03-23 17:09:58 +0000

    KaddyLines for future use. Kaddy doesn't work yet

--- KaddyLines.hs
+++ KaddyLines.hs
@@ -14,7 +14,7 @@ main' = do (progFile : _) <- getArgs -- Read in the program
            let parsedProg = parseCalc sourceTokens -- Parsed program
            inputStream <- getContents -- Read in the input stream
            let cLines = lines inputStream -- Split the stream into lines
-           evalInput (cLines !! 0) prog
+           evalInput cLines parsedProg 
            hPutStr stdout "End"
         
 
@@ -22,4 +22,8 @@ noParse :: ErrorCall -> IO ()
 noParse e = do let err =  show e
                hPutStr stderr err
                return ()
+
+evalInput :: [String] -> [Expr] -> Enviroment -> IO ()
+evalInput ((l:[]), prog, env) = hPutStr stdout result
+                              where result = evLoop
 
